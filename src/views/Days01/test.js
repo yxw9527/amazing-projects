e97569b7030c599f3528a0f5a3e8ed7459a9041c
yuanxuewei <yxw8462531.YXW>
2022-04-05 13:30:19 +0800

    '联系day01'

---
+++ src/views/Days01/test.js
@@ -0,0 +1,7 @@
+var str = '章蕾是个狗屎！';
+console.log(str);
+function sayHello(person) {
+    return 'hello' + person;
+}
+var user = false;
+console.log(sayHello(user));
---
+++ src/views/Days01/test.ts
@@ -0,0 +1,7 @@
+const str: string = '章蕾是个狗屎！'
+console.log(str)
+function sayHello(person: string) {
+    return 'hello'+person
+}
+let user = false
+console.log(sayHello(user))
